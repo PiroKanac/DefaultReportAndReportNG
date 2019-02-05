26c2a7228edfb4243c23eee1c6c1786f528ed690
PiroKanac <runyoshimi@live.com>
2019-02-05 08:30:32 +0100

    Default reporting

---
+++ DefaultReport/src/main/java/testcases/TC_002.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+
+	@Test
+	public void tc_001() {
+
+		System.out.println("*******************");
+		System.out.println("launching Chrome browser");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vnikolic\\Downloads\\Selenium\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize(); 
+		driver.get("https://google.com");
+		System.out.println("Navigate to URL");
+		driver.findElement(By.name("q")).sendKeys("Hello Selenium!");
+		Assert.assertTrue(true);
+		driver.quit();
+
+	}
+}
